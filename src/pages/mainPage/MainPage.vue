cbe35c22524589f1a0b3f5ed0b3590df64c11424
Vera869 <verea8910@gmail.com>
2024-08-06 18:05:37 +0700

    + addTask

--- src/pages/mainPage/MainPage.vue
+++ src/pages/mainPage/MainPage.vue
@@ -1,19 +1,53 @@
 <script setup lang="ts">
 import {ref} from 'vue';
 import { Task, taskList } from './mockData.ts';
+import AddTaskForm from './components/addTaskComponent/AddTaskForm.vue';
 
 const tasks: Task[] = ref(taskList);
-console.log(tasks);
+
+const onAddTask = (title: string, discription: string) => {
+   const newTask: Task = {
+      id: tasks.value.length + 1,
+      title: title,
+      discription: discription,
+      completed: false,
+   };
+   tasks.value = [...tasks.value, newTask];
+}
 </script>
 
 <template>
-   <h1>Список задач</h1>
-   <div>
-      <ul>
-         <li v-for="task in tasks" v-bind:key="task.id">{{ task.title }}</li>
-      </ul>
+   <div class="main">
+      <h1 class="main_header">Список задач</h1>
+      <div class="main_tasks">
+         <ul class="tasks">
+            <li class="tasks_title" v-for="task in tasks" v-bind:key="task.id">{{ task.title }}
+               <p class="tasks_discript">{{ task.discription }}</p>
+            </li>
+         </ul>
+      </div>
+      <AddTaskForm :onAddTask="onAddTask" />
    </div>
 </template>
 
-<style>
+<style scoped>
+.main {
+   width: 70vw;
+   background: hsla(52, 90%, 51%, 0.5) ;
+   color: darkgreen;
+   border: 2px solid green;
+   border-radius: 20px;
+   padding: 20px 40px;
+   display: block;
+}
+.main_tasks {
+   width: 40vw;
+   height: auto;
+}
+.tasks_title, .tasks_discript {
+  width: 60vw;
+  padding: 3px;
+  word-wrap: break-word;
+}
+
 </style>
---
+++ src/pages/mainPage/components/addTaskComponent/AddTaskForm.vue
@@ -0,0 +1,85 @@
+<script setup lang="ts">
+import {ref} from 'vue';
+
+const props = defineProps<{
+   onAddTask:(taskTitle: string) => void;
+}>()
+
+const inputTitle = ref("");
+const inputDiscription = ref("");
+
+const handleTitleChange = (e: Event) => {
+   const target = e.target as HTMLInputElement;
+   inputTitle.value = target.value;
+}
+
+const handleDiscriptionChange = (e: Event) => {
+   const target = e.target as HTMLInputElement;
+   inputDiscription.value = target.value;
+}
+
+const addTaskHendle = () => {
+   props.onAddTask(inputTitle.value, inputDiscription.value);
+   inputTitle.value = "";
+   inputDiscription.value = "";
+}
+</script>
+
+<template>
+   <div class="form">
+      <input class="form_input" 
+         @change="handleTitleChange" 
+         type="text" 
+         placeholder="название задачи" 
+         :value="inputTitle" 
+      />
+      <input class="form_input" 
+         @change="handleDiscriptionChange" 
+         type="text" 
+         placeholder="описание задачи" 
+         :value="inputDiscription" 
+      />
+      <button @click="addTaskHendle" class="form_button">Добавить</button>
+   </div>
+</template>
+
+<style scoped>
+.form {
+   display: flex;
+   flex-direction: column;
+   gap: 12px;
+   margin: 20px;
+}
+.form_input {
+   width: 30vw;
+   height: 3vw;
+   outline:none;
+   border: none;
+   border-radius: 20px;
+   border-bottom: 2px solid green;
+   background: hsla(52, 90%, 51%, 0.5);
+   color: green;
+   padding: 1rem;
+   &:action, hover, focus {
+      border-color: green;
+   }
+}
+.form_button {
+   width: 20vw;
+   height: auto;
+   outline:none;
+   border: none;
+   border: 2px solid green;
+   border-radius: 20px;
+   background: hsla(52, 90%, 51%, 0.5);
+   color: green;
+   padding: 1rem;
+   &:hover {
+      background: greenyellow;
+   }
+   &:active {
+      background:green;
+   }
+}
+
+</style>
